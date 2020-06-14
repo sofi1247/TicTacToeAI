3d13a11f1efdea302be5883b7918a121c19fb1c1
sofi1247 <sofi1247@gmail.com>
2020-06-14 20:30:18 +0200

    updated with AI1

--- Main.py
+++ Main.py
@@ -2,8 +2,9 @@ import TicTacToe
 import Player
 
 human1_test = Player.Human("x") # Player -> File; Human -> Class
-human2_test = Player.Human("o")
-object1 = TicTacToe.TicTacToe( human1_test, human2_test) # 1st TTT -> FileName; 2nd TTT -> Class name
+ais_test = Player.AI("o")
+#human2_test = Player.Human("o")
+object1 = TicTacToe.TicTacToe(human1_test, ais_test) # 1st TTT -> FileName; 2nd TTT -> Class name
 
 #object1.reset_game()
 #object1.print_score()
